552f460ad4c7375bb9ebb19831011b7c1f0b7f20
vamseemanchineni <vamsimanchineni26@gmail.com>
2024-07-17 21:37:17 +0530

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "student",
+                DateOfBirth = new DateTime(1954,12,01)
+            };
+            return View(data);
+        }
+    }
+}
